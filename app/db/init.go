a149ac9619900dab082bf6b62b1ff79e122d4c88
Paweł Szulik <paul.szulik@gmail.com>
2021-03-20 18:13:16 +0100

    Move user related data logic to separate file.
    Signed-off-by: Paweł Szulik <paul.szulik@gmail.com>

--- app/db/init.go
+++ app/db/init.go
@@ -2,11 +2,7 @@ package db
 
 import (
 	"context"
-	"errors"
 	"fmt"
-	"go.mongodb.org/mongo-driver/bson"
-	"golang.org/x/crypto/bcrypt"
-	"natka/app/models"
 	"time"
 
 	"go.mongodb.org/mongo-driver/mongo"
@@ -31,69 +27,4 @@ func init() {
 	if err != nil {
 		fmt.Printf("%v", err)
 	}
-}
-
-func InsertUser(user models.User) error {
-	ctx, _ := context.WithTimeout(context.Background(), connectionTimeout)
-
-	db := client.Database(databaseName)
-	col := db.Collection(userCollection)
-	if col == nil {
-		return errors.New("nil collection")
-	}
-
-	_, err := col.InsertOne(ctx, &user)
-	if err != nil {
-		return err
-	}
-
-	return nil
-}
-
-func GetUser(mail string) (*models.User, error) {
-	ctx, _ := context.WithTimeout(context.Background(), connectionTimeout)
-
-	db := client.Database(databaseName)
-	col := db.Collection(userCollection)
-
-	result := col.FindOne(ctx, bson.D{{"mail", mail}})
-	if result.Err() != nil {
-		return nil, result.Err()
-	}
-
-	user := models.User{}
-	err := result.Decode(&user)
-	if err != nil {
-		return nil, err
-	}
-
-	return &user, nil
-}
-
-func CheckMail(mail string) (bool, error) {
-	_, err := GetUser(mail)
-	if err != nil {
-		if err == mongo.ErrNoDocuments {
-			return false, nil
-		}
-		return false, err
-	}
-
-	return true, nil
-}
-
-func Login(mail, password string) (*models.User, error) {
-	user, err := GetUser(mail)
-	if err != nil {
-		return nil, err
-	}
-	if user != nil {
-		err = bcrypt.CompareHashAndPassword(user.Password, []byte(password))
-		if err != nil {
-			return nil, err
-		}
-		return user, nil
-	}
-
-	return nil, nil
 }
