814faff3724c48f0c1d9e6c48169317e11f7b6e4
Paweł Szulik <paul.szulik@gmail.com>
2021-04-22 19:16:03 +0200

    Validate profile image.
    Signed-off-by: Paweł Szulik <paul.szulik@gmail.com>

--- app/controllers/profile/profile.go
+++ app/controllers/profile/profile.go
@@ -1,7 +1,10 @@
 package profile
 
 import (
+	"bytes"
 	"encoding/base64"
+	img "image"
+	_ "image/jpeg"
 
 	"github.com/revel/revel"
 
@@ -11,6 +14,12 @@ import (
 	"natka/app/db"
 	"natka/app/models"
 	"natka/app/routes"
+)
+
+const (
+	_      = iota
+	KB int = 1 << (10 * iota)
+	MB
 )
 
 type Profile struct {
@@ -55,24 +64,36 @@ func (c Profile) Edit() revel.Result {
 func (c Profile) ApplyEdit(user models.User, avatar []byte) revel.Result {
 	if sessionUser := utils.IsConnected(c.Session); sessionUser != nil {
 		sessionUser.Name = user.Name
-		if len(avatar) != 0 {
-			image := models.Image{Data: avatar}
-			if sessionUser.Avatar == nil {
-				id, err := db.InsertImage(image)
-				var another = id.(primitive.ObjectID)
-				sessionUser.Avatar = &another
-				if err != nil {
-					return c.RenderError(err)
-				}
-			} else {
-				image.ID = sessionUser.Avatar
-				err := db.EditImage(image)
-				if err != nil {
-					return c.RenderError(err)
-				}
+		c.Validation.Required(avatar)
+		c.Validation.MinSize(avatar, 2*KB)
+		c.Validation.MaxSize(avatar, 4*MB)
+		rawImage, format, err := img.DecodeConfig(bytes.NewReader(avatar))
+		c.Validation.Required(err == nil).Key("avatar").Message("Incorrect file format")
+		c.Validation.Min(rawImage.Height, 600).Message("Minimum avatar size is 600x600")
+		c.Validation.Min(rawImage.Width, 600).Message("Minimum avatar size is 600x600")
+		c.Validation.Required(format == "jpeg").Key("avatar").Message("JPEG format is required")
+		if c.Validation.HasErrors() {
+			c.Validation.Keep()
+			c.FlashParams()
+			return c.Redirect(routes.Profile.Edit())
+		}
+		// TODO: Shrink image.
+		image := models.Image{Data: avatar}
+		if sessionUser.Avatar == nil {
+			id, err := db.InsertImage(image)
+			var another = id.(primitive.ObjectID)
+			sessionUser.Avatar = &another
+			if err != nil {
+				return c.RenderError(err)
+			}
+		} else {
+			image.ID = sessionUser.Avatar
+			err := db.EditImage(image)
+			if err != nil {
+				return c.RenderError(err)
 			}
 		}
-		err := db.EditUser(*sessionUser)
+		err = db.EditUser(*sessionUser)
 		if err != nil {
 			return c.RenderError(err)
 		}
