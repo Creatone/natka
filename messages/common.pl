0d57d82e3fa0fbfa58857488db3131d38be45f13
Paweł Szulik <paul.szulik@gmail.com>
2021-06-26 14:39:39 +0200

    Rename messages files.
    Signed-off-by: Paweł Szulik <paul.szulik@gmail.com>

---
+++ messages/common.pl
@@ -0,0 +1,19 @@
+[DEFAULT]
+
+# NAVBAR
+navbar.home=Strona główna
+navbar.articles=Artykuły
+navbar.calculator=Kalkulator KCAL
+navbar.login=Zaloguj
+navbar.about=O mnie
+navbar.contact=Kontakt
+
+about.me=Cześć! Jestem Natalia
+contact=Kontakt
+
+# FOOTER
+footer.text =@ 2021 - Dietetyk Natka. Wszelkie prawa zastrzeżone.
+
+# BUTTONS
+btn.cancel=Anuluj
+btn.submit=Potwierdź
