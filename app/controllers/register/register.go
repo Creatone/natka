747c129812fc7f269e0c13eddd60eb189c3eef89
Paweł Szulik <paul.szulik@gmail.com>
2021-06-26 16:59:13 +0200

    Logged user should not be able to register.
    Signed-off-by: Paweł Szulik <paul.szulik@gmail.com>

--- app/controllers/register/register.go
+++ app/controllers/register/register.go
@@ -17,18 +17,37 @@ type Register struct {
 }
 
 func (c Register) Index(mail string) revel.Result {
+	sessionUser, err := c.Session.Get("user")
+	if err != nil {
+		c.Validation.Error(err.Error())
+		c.Validation.Keep()
+		return c.Redirect(routes.App.Index())
+	}
+
+	if sessionUser != nil {
+		return c.Redirect(routes.App.Index())
+	}
+
 	return c.Render(mail)
 }
 
 func (c Register) Register(mail, name, password string) revel.Result {
-	c.Session.Get("user")
+	sessionUser, err := c.Session.Get("user")
+	if err != nil {
+		c.Validation.Error(err.Error())
+		c.Validation.Keep()
+		return c.Redirect(routes.App.Index())
+	}
+
+	if sessionUser != nil {
+		return c.Redirect(routes.App.Index())
+	}
 
 	user := models.User{
 		Name: name,
 		Mail: mail,
 	}
 
-	var err error
 	user.Password, err = bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
 	if err != nil {
 		c.Log.Errorf("cannot encrypt %q password: %v", user.Name, err)
